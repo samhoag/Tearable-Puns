ea3a11d0071e088f4d45079d52b09f49eca84eb4
samhoag <samhoag@gmail.com>
2024-02-09 20:02:25 -0800

    Initial Commit

---
+++ Useless Facts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Useless Facts
+//
+//  Created by Sam Hoag on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Useless Facts/Useless_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Useless_FactsApp.swift
+//  Useless Facts
+//
+//  Created by Sam Hoag on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Useless_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
